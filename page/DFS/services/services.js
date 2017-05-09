c80eecaa6962e28e2d7b0ab312d2b5fb002f85bf
Huysh3 <734645956@qq.com>
2017-05-10 02:04:31 +0800

    dev

--- page/DFS/services/services.js
+++ page/DFS/services/services.js
@@ -4,7 +4,7 @@ Page({
   },
   phoneCall: function() {
     wx.makePhoneCall({
-      phoneNumber: '13602455397' //仅为示例，并非真实的电话号码
+      phoneNumber: '4006901078' //仅为示例，并非真实的电话号码
     })
   },
   routerGoHome: function() {
